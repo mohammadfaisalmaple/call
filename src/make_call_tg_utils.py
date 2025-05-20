5e98de78a3f2a633a891f6b520608340775d6e4a
mohammadfaisalmaple <m.faisal@mapletele.com>
2025-05-20 15:15:43 +0300

    Edit done

--- src/make_call_tg_utils.py
+++ src/make_call_tg_utils.py
@@ -270,6 +270,8 @@ am start -n org.telegram.messenger/org.telegram.ui.LaunchActivity
 
     # Monitor calls using BaresipManager
     trace_logger.info("[make_telegram_call] Monitoring Telegram call logs")
+    trace_logger.info("monitor_telegram_calls monitor_telegram_calls ")
+
     monitor_telegram_calls(sip_manager, emulator_port=emulator_port, output_file=None)
 
     sip_manager.ensure_connected()
