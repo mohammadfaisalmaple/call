d63004a9a039089660886d1709e9f05170da5f2b
mohammadfaisalmaple <m.faisal@mapletele.com>
2025-05-20 18:29:51 +0300

    ُedit code done

--- src/make_call_tg_utils.py
+++ src/make_call_tg_utils.py
@@ -258,12 +258,15 @@ am start -n org.telegram.messenger/org.telegram.ui.LaunchActivity
             return False
         return True
 
+    run([ADB, "-s", device_id, "shell", "input", "keyevent", "4"])
     trace_logger.info("[make_telegram_call] Step: FIRST 'Call' button")
     if not find_and_tap("Call"):
         return False
     trace_logger.info("[make_telegram_call] Step: 'End Call' button")
     if not find_and_tap("End Call"):
         return False
+    
+    run([ADB, "-s", device_id, "shell", "input", "keyevent", "4"])
     trace_logger.info("[make_telegram_call] Step: SECOND 'Call' button")
     if not find_and_tap("Call"):
         return False
